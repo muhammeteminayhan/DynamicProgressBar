f410d2d33ea2b28a47190dff3f4da04d7df90a88
Muhammet Emin Ayhan <aemin8343@gmail.com>
2024-10-15 20:17:43 +0300

    Initial Commit

---
+++ DynamicProgressBar/DynamicProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicProgressBarApp.swift
+//  DynamicProgressBar
+//
+//  Created by Muhammet Emin Ayhan on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DynamicProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
